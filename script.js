5b398898ab8dcedd661a714c64a61df8a6c798ea
alvaropaxql <alvaropasnu@gmail.com>
2021-09-05 11:51:44 +0200

    v1.2: api requests working

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const wrapper = document.querySelector(".wrapper");
 const inputSection = wrapper.querySelector(".input-section");
 const infoTxt = inputSection.querySelector(".info-txt");
 const inputField = inputSection.querySelector("input");
+const locationButton = inputSection.querySelector("button");
+
+let pai;
 
 inputField.addEventListener("keyup", e => {
     if (e.key === "Enter" && inputField.value !== "") {
@@ -9,6 +12,53 @@ inputField.addEventListener("keyup", e => {
     }
 });
 
+locationButton.addEventListener("click", () => {
+    if (navigator.geolocation) { // if supported by the browser
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    } else {
+        alert("Your browser does not support the geolocation api.");
+    }
+});
+
+function onSuccess(position) {
+    const {
+        latitude,
+        longitude
+    } = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${config.apiKey}`;
+    fecthData();
+}
+
+function onError(error) {
+    infoTxt.textContent = error.message;
+    infoTxt.classList.add("error");
+}
+
 function requestApi(city) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={API key}`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${config.apiKey}`;
+    fecthData();
+}
+
+function fecthData() {
+    infoTxt.textContent = "Getting weather details...";
+    infoTxt.classList.add("pending");
+    /*after getting the api response, it is parsed into an object and passed as an argument to another function via then() */
+    fetch(api).then(response => response.json().then(result => weatherDetails(result)));
+}
+
+function weatherDetails(info) {
+    if (info.cod === "404") {
+        infoTxt.classList.replace("pending", "error");
+        infoTxt.textContent = `${inputField.value} is not a valid city name.`;
+    } else {
+        // get propierties
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {feels_like, humidity, temp} = info.main;
+
+        infoTxt.classList.remove("pending", "error");
+        wrapper.classList.add("active");
+        console.log(info);
+    }
 }
